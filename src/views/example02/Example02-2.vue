2d19bcbfcf826bbe7c8daae2b77cf8bb09fe82bc
yue24413 <2312400876@qq.com>
2024-07-14 14:23:01 +0800

    experiment2

--- src/views/example02/Example02-2.vue
+++ src/views/example02/Example02-2.vue
@@ -1,40 +1,62 @@
 <template>
-  <div>
+  <div class="main">
     <!-- 显示总学分，颜色根据是否达到条件而变化 -->
     <div :style="{ color: totalCredit >= targetCredit ? 'green' : 'red' }">
       总学分: {{ totalCredit }} / {{ targetCredit }}
     </div>
     <!-- 显示所有课程 -->
-    <div v-for="course in sortedCourses" :key="course.id">
-      <label>
-        <input type="checkbox" v-model="selectedCourses[course.id]" />
+    <div class="allCourse">
+      <label v-for="course of sortedCourses" :key="course.id">
+        <input type="checkbox" v-model="selectedCourses" :value="course" />
         {{ course.name }} - ({{ course.point }}学分) - {{ course.semester }}
       </label>
     </div>
-
+    <br />
+    <br />
     <!-- 已选课程 -->
-    <ul>
-      <li v-for="course in selectedSortedCourses" :key="course.id">
-        {{ course.name }} ({{ course.point }}学分) - {{ course.semester }}
-      </li>
-    </ul>
+    <div class="selCourse">
+      <ul>
+        <li v-for="course of selectedSortedCourses" :key="course.id">
+          {{ course.name }} - ({{ course.point }}学分) - {{ course.semester }}
+        </li>
+      </ul>
+    </div>
+    <br />
   </div>
 </template>
 <script lang="ts" setup>
 import { listCourses } from '@/datasource/DataSource'
 import type { Course } from '@/type'
 import { computed, ref } from 'vue'
-const targetCredit = 17.5
-const selectedCourses = ref({}) /**初始化空对象 */
-
+const targetCredit = 8.5
 const sortedCourses = computed(() => {
   return [...listCourses()].sort((a, b) => a.semester - b.semester)
 })
+console.log(sortedCourses)
+console.log(sortedCourses.value)
+
+const selectedCourses = ref<Course[]>([]) /**初始化空对象 显式声明类型 */
 
 const selectedSortedCourses = computed(() => {
-  return sortedCourses.value.filter((course) => selectedCourses.value[course.id])
+  return [...selectedCourses.value].sort((a, b) => (a.semester || 0) - (b.semester || 0))
 })
 const totalCredit = computed(() => {
-  return selectedSortedCourses.value.reduce((acc, course) => acc + course.point, 0)
+  return selectedCourses.value.reduce((acc, course) => acc + (course.point || 0), 0)
 })
 </script>
+
+<style scoped>
+.allCourse {
+  width: 300px;
+  display: inline-block;
+  border: 1px red solid;
+}
+.allCourse > label {
+  display: inline-block;
+  width: 100%;
+}
+.selCourse {
+  display: inline-block;
+  border: 1px red solid;
+}
+</style>
