787643eb642bd2b4eaf9dced05ba9c7f973c5563
yue24413 <2312400876@qq.com>
2024-07-25 20:02:43 +0800

    example2

--- src/views/example02/Example02-2.vue
+++ src/views/example02/Example02-2.vue
@@ -17,15 +17,15 @@
     <!-- 已选课程 -->
     <div class="selCourse">
       <ul>
-        <li v-for="course of selectedSortedCourses" :key="course.id">
+        <li v-for="course of selectedCourses" :key="course.id">
           {{ course.name }} - ({{ course.point }}学分) - {{ course.semester }}
         </li>
       </ul>
     </div>
     <br />
-    <!-- {{ selectedCourses }}
+    {{ selectedCourses }}
     <br />
-    <br /> -->
+    <br />
     <!-- {{ sortedAllCourses }}
     <br />
     <br />
@@ -43,30 +43,33 @@ const sortedAllCourses = [...listCourses()].sort((a, b) => a.semester - b.semest
 
 const selectedCourses = ref<Course[]>([]) /**初始化空对象 显式声明类型 */
 
-const selectedSortedCourses = ref<Course[]>([])
 const totalCredit = ref(0)
 
 watch(
-  selectedCourses,
-  (newCou, oldCou) => {
-    selectedSortedCourses.value = [...newCou].sort((a, b) => (a.semester || 0) - (b.semester || 0))
-    /**不可在 watch 回调中修改 selectedCourses直接进行排序，因为正在监听 selectedCourses 的变化 */
-    totalCredit.value = newCou.reduce((acc, course) => acc + (course.point || 0), 0)
-  },
-  { deep: true }
+  /**箭头函数做参数,监听selectedCourses.length */
+  /**第一个参数应该是响应式引用或者是一个 getter 函数 */
+  () => selectedCourses.value.length,
+  (newLength, oldLength) => {
+    if (newLength !== oldLength) {
+      // 长度发生变化时,执行
+      /**数组的sort对自身排序有，与集合区分 */
+      selectedCourses.value.sort((a, b) => (a.semester || 0) - (b.semester || 0))
+      totalCredit.value = selectedCourses.value.reduce(
+        (acc, course) => acc + (course.point || 0),
+        0
+      )
+    }
+  }
 )
 
-/****************************************************** */
-// const selectedSortedCourses = computed(() =>
-//   [...selectedCourses.value].sort((a, b) => (a.semester || 0) - (b.semester || 0))
-// )
+// watch(selectedCourses, (newCou, oldCou) => {
+//   selectedCourses.value = [...newCou].sort((a, b) => (a.semester || 0) - (b.semester || 0))
+//   /**不可在 watch 回调中修改 selectedCourses直接进行排序，因为正在监听 selectedCourses 的变化 */
+//   totalCredit.value = newCou.reduce((acc, course) => acc + (course.point || 0), 0)
+// })
 
-// const totalCredit = computed(() =>
-//   selectedCourses.value.reduce((acc, course) => acc + (course.point || 0), 0)
-// )
-/**   , 0)  表示累加器的初始值为0 */
-
-/******************************************************* */
+/*直接监听响应式引用：如果你需要监听整个响应式对象或数组的所有变化，可以直接传入这个引用。
+   使用 getter 函数：如果你只关心某个特定属性的变化，如长度或其他特定属性，应该使用 getter 函数。 */
 </script>
 
 <style scoped>
