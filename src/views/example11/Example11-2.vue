5e90bdbde2f858b1ff782e88c10432c87489d5df
yue24413 <2312400876@qq.com>
2024-08-07 19:08:32 +0800

    example11

---
+++ src/views/example11/Example11-2.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div>
+      <h3>Service & Store</h3>
+      <p>
+        在业务层发出网络请求；登录成功将响应结果user对象置于store，将响应头中token置于sessionstorage。
+        <br />
+        再次发出请求时，从sessionstorage中取出数据再请求中添加在header一起发出。
+      </p>
+      <br />
+      1001/123456
+    </div>
+    <div>
+      <input type="text" v-model="userForm.number" />
+      <br />
+      <input type="password" v-model="userForm.password" />
+      <br />
+      <button title="button" @click="loginF">Login</button>
+      <br />
+      {{ userR?.title }}
+    </div>
+    <div>
+      <p>
+        未登录发出的请求会返回401未登录业务码；token错误返回403无权限业务码，被请求拦截器拦截后激活自定义模态框，弹出错误信息。
+      </p>
+      <button type="button" @click="getHome">home</button>
+      <br />
+      <button @click="clearSessionStorageF">清空SessionStorage用于测试</button>
+      <br />
+      <ul>
+        <li v-for="(c, index) of coursesR" :key="index">{{ c.name }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+import { useExample11Store } from './Example11Store'
+import { getHomeService, loginService } from './Example11Service'
+
+interface User {
+  number: string
+  password: string
+}
+
+const userForm = ref<User>({ number: '', password: '' })
+const userR = useExample11Store().userS
+const coursesR = useExample11Store().coursesS
+const getHome = async () => {
+  await getHomeService()
+}
+const loginF = async () => {
+  if (userForm.value.number.length == 0 || userForm.value.password.length == 0) {
+    throw '账号密码不能为空！'
+  }
+  const user: User = {
+    number: userForm.value.number,
+    password: userForm.value.password
+  }
+  await loginService(user)
+  userForm.value.number = ''
+  userForm.value.password = ''
+}
+const clearSessionStorageF = () => {
+  sessionStorage.clear()
+  coursesR.value.length = 0
+}
+</script>
--- src/views/example11/Example11Service.ts
+++ src/views/example11/Example11Service.ts
@@ -7,3 +7,19 @@ export const listCoursesService = async (userId: number) => {
   const { data } = await useGet<{ courses: Course[] }>(`users/${userId}/courses`)
   return data.value?.data.courses ?? []
 }
+//loginService 函数接收一个对象作为参数，该对象有两个属性：number 和 password
+export const loginService = async (user: { number: string; password: string }) => {
+  //调用 usePost 函数时，期望的返回类型为 { user: User }
+  const resp = await usePost<{ user: User }>('login', user)
+  //从响应头部中提取 token，存储在 sessionStorage 中。为了后续请求中使用这个 token 来进行认证
+  const token = resp.response.value?.headers.get('token')
+  token && sessionStorage.setItem('token', token)
+  const userS = useExample11Store().userS
+  resp.data.value?.data && (userS.value = resp.data.value.data.user)
+}
+
+// example11-2
+export const getHomeService = async () => {
+  const resp = await useGet<{ courses: Course[] }>('home')
+  useExample11Store().coursesS.value = resp.data.value?.data.courses ?? []
+}
